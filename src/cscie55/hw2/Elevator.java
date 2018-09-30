1cf0d674e35619b662355a2de5f673e25abcab2e
Robert Chang <robccorp@gmail.com>
2018-09-30 09:53:38 -0400

    moved order of things around, only failing 1 test disembark

--- src/cscie55/hw2/Elevator.java
+++ src/cscie55/hw2/Elevator.java
@@ -44,37 +44,6 @@ public class Elevator {
 */
     public void move() {
 
-        myBuilding.getFloor(this.currentFloor).clearNumPass();
-
-        int numWaitingOnFloor = myBuilding.getFloor(this.currentFloor).getPassengersWaiting();
-        // int spareCapacity = CAPACITY - this.getPassengers();
-        // System.out.println("Num Waiting: " + numWaitingOnFloor + " Spare: " + spareCapacity + " Num inside: " + this.getPassengers());
-        // int numToBoard = Math.min(spareCapacity, numWaitingOnFloor);    // will board the number of people of waiting, or the spare capacity, whichever is the lesser value
-        if(numWaitingOnFloor > 0 && this.getPassengers() < CAPACITY) {  // loop to board the persons waiting on a floor 
-            // System.out.println("Inside for loop");
-            for (int i = 0; i < numWaitingOnFloor; i++) {                      // will only board waiting people up to the capacity limit
-                try {
-                    if (this.currentFloor == 1) {                       // instructions state waiting passengers on
-                        this.boardPassenger(Building.FLOORS);           // first floor go to higher floor
-                        myBuilding.getFloor(this.currentFloor).clearIsWaitingArray(); // for each person boarded, decrement waiting array   
-                        // System.out.println("Inside Try Block");
-                    }                                           
-                    else {
-                        // System.out.println("Inside else " + this.getPassengers());
-                        this.boardPassenger(1);                         // people waiting on higher floors go to floor 1.
-                        myBuilding.getFloor(this.currentFloor).clearIsWaitingArray(); // for each person boarded, decrement waiting array  
-                    }
-                    // System.out.println("Exit else " + this.getPassengers());
-                }
-                catch(ElevatorFullException e) {
-                    // System.out.println("Inside catch " + this.getPassengers());
-                    break;
-                    // this.move();
-                }
-            }
-            
-        }
-
         System.out.println(this.toString());
 
         // Next block of code is unchanged from HW1, basic move functionality
@@ -94,6 +63,42 @@ public class Elevator {
             this.currentFloor--;
         }
 
+        int numWaitingOnFloor = myBuilding.getFloor(this.currentFloor).getPassengersWaiting();
+        int spareCapacity = CAPACITY - this.getPassengers();
+        // System.out.println("Floor: " + getCurrentFloor() + " Num Waiting: " + numWaitingOnFloor + " Spare: " + spareCapacity + " Num inside: " + this.getPassengers());
+
+        myBuilding.getFloor(this.currentFloor).clearNumPass();
+
+        // System.out.println("Floor: " + getCurrentFloor() + " Num Waiting: " + numWaitingOnFloor + " Spare: " + spareCapacity + " Num inside: " + this.getPassengers());
+
+        
+        // System.out.println("Floor: " + getCurrentFloor() + " Num Waiting: " + numWaitingOnFloor + " Spare: " + spareCapacity + " Num inside: " + this.getPassengers());
+        int numToBoard = Math.min(spareCapacity, numWaitingOnFloor);    // will board the number of people of waiting, or the spare capacity, whichever is the lesser value
+        if(numWaitingOnFloor > 0 && this.getPassengers() < CAPACITY) {  // loop to board the persons waiting on a floor 
+            System.out.println("Inside for loop");
+            for (int i = 0; i < numWaitingOnFloor; i++) {                      // will only board waiting people up to the capacity limit
+                try {
+                    if (this.currentFloor == 1) {                       // instructions state waiting passengers on
+                        this.boardPassenger(Building.FLOORS);           // first floor go to higher floor
+                        myBuilding.getFloor(this.currentFloor).clearIsWaitingArray(); // for each person boarded, decrement waiting array   
+                        System.out.println("Inside Try Block");
+                    }                                           
+                    else {
+                        System.out.println("Inside else " + this.getPassengers());
+                        this.boardPassenger(1);                         // people waiting on higher floors go to floor 1.
+                        myBuilding.getFloor(this.currentFloor).clearIsWaitingArray(); // for each person boarded, decrement waiting array  
+                    }
+                    System.out.println("Exit else " + this.getPassengers());
+                }
+                catch(ElevatorFullException e) {
+                    System.out.println("Inside catch " + this.getPassengers());
+                    break;
+                    // this.move();
+                }
+            }
+            
+        }
+
     }
 
 /**
@@ -103,7 +108,7 @@ public class Elevator {
     public void boardPassenger(int destinationFloorNumber) throws ElevatorFullException {
         if(this.getPassengers() >= CAPACITY) {
             myBuilding.getFloor(this.currentFloor).waitForElevator(); // passengers who can't board because of full capacity get placed in the wait for elevator queue
-            // System.out.println("Inside Exception");
+            System.out.println("Inside Exception");
             throw new ElevatorFullException("Elevator is at full capacity. Please wait for the elevator to return.");
         }
         else {
--- src/cscie55/hw2/myTest.java
+++ src/cscie55/hw2/myTest.java
@@ -28,79 +28,78 @@ public class myTest {
         // System.out.println("End Checking movement only");
 
     // Check that passengers get on and off correctly.
-    
-        // Building building = new Building();
-        // Elevator elevator = building.getElevator();
-        // // Add passengers and disembark them on the way up
-        // System.out.println("Checking Disembark");
-        // try {
-        //     elevator.boardPassenger(3);
-        // }
-        // catch(ElevatorFullException e) {
-        //     elevator.move();
-        // }
-        // try {
-        //     elevator.boardPassenger(4);
-        // }
-        // catch(ElevatorFullException e) {
-        //     elevator.move();
-        // }
-        // try {
-        //     elevator.boardPassenger(4);
-        // }
-        // catch(ElevatorFullException e) {
-        //     elevator.move();
-        // }
-        // try {
-        //     elevator.boardPassenger(6);
-        // }
-        // catch(ElevatorFullException e) {
-        //     elevator.move();
-        // }
-        // try {
-        //     elevator.boardPassenger(6);
-        // }
-        // catch(ElevatorFullException e) {
-        //     elevator.move();
-        // }
-        // try {
-        //     elevator.boardPassenger(6);
-        // }
-        // catch(ElevatorFullException e) {
-        //     elevator.move();
-        // }
-        // elevator.move();
-        // elevator.move();
-        // elevator.move();
-        // elevator.move();
-        // elevator.move();
-        // elevator.move();
-        // System.out.println("End Checking Disembark");
-    
-
-    // Check that passengers on higher floors can call and board the elevator, and then
-    // disembark on the ground floor.
         Building building = new Building();
         Elevator elevator = building.getElevator();
-        System.out.println("Checking higher floors can call and board");
-        building.getFloor(3).waitForElevator();
-        building.getFloor(3).waitForElevator();
-        building.getFloor(6).waitForElevator();
-        building.getFloor(6).waitForElevator();
-        building.getFloor(6).waitForElevator();
+        // Add passengers and disembark them on the way up
+        System.out.println("Checking Disembark");
+        try {
+            elevator.boardPassenger(3);
+        }
+        catch(ElevatorFullException e) {
+            elevator.move();
+        }
+        try {
+            elevator.boardPassenger(4);
+        }
+        catch(ElevatorFullException e) {
+            elevator.move();
+        }
+        try {
+            elevator.boardPassenger(4);
+        }
+        catch(ElevatorFullException e) {
+            elevator.move();
+        }
+        try {
+            elevator.boardPassenger(6);
+        }
+        catch(ElevatorFullException e) {
+            elevator.move();
+        }
+        try {
+            elevator.boardPassenger(6);
+        }
+        catch(ElevatorFullException e) {
+            elevator.move();
+        }
+        try {
+            elevator.boardPassenger(6);
+        }
+        catch(ElevatorFullException e) {
+            elevator.move();
+        }
         elevator.move();
         elevator.move();
         elevator.move();
         elevator.move();
         elevator.move();
         elevator.move();
-        elevator.move();
-        elevator.move();
-        elevator.move();
-        elevator.move();
-        elevator.move();
-        elevator.move();
-        System.out.println("End checking higher floors can call and board");
+        System.out.println("End Checking Disembark");
+    
+
+    // // Check that passengers on higher floors can call and board the elevator, and then
+    // // disembark on the ground floor.
+    //     Building building = new Building();
+    //     Elevator elevator = building.getElevator();
+    //     System.out.println("Checking higher floors can call and board");
+    //     building.getFloor(3).waitForElevator();
+    //     building.getFloor(3).waitForElevator();
+    //     building.getFloor(6).waitForElevator();
+    //     building.getFloor(6).waitForElevator();
+    //     building.getFloor(6).waitForElevator();
+    //     elevator.move();
+    //     elevator.move();
+    //     elevator.move();
+    //     elevator.move();
+    //     elevator.move();
+    //     elevator.move();
+    //     elevator.move();
+    //     elevator.move();
+    //     elevator.move();
+    //     elevator.move();
+    //     elevator.move();
+    //     elevator.move();
+    //     System.out.println("End checking higher floors can call and board");
 
     // // Check handling of a full elevator.
     //     Building building = new Building();
